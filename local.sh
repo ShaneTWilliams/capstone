5948d66567cc4ce6e0925e2e8d15372068b48333
Shane Williams <twilliams@mun.ca>
2024-04-02 11:36:16 -0230

    Clean up old files

--- local.sh
+++
@@ -1,8 +0,0 @@
-gcc \
-    local.c software/firmware/src/app/generated/lm75b.c \
-    -o local \
-    -Isoftware/firmware/src/app/generated \
-    -Isoftware/firmware/src/app \
-    -Isoftware/firmware/src
-
-./local
